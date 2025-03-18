5f5be011d02e888f9dd8a285d12add48a375af45
Leonardo Oliveira Balsalobre <oliveirabalsa2@gmail.com>
2025-03-18 07:18:31 -0300

    fix: remove unused logs

--- main.go
+++ main.go
@@ -10,19 +10,15 @@ import (
 )
 
 func main() {
-	// Load .env file
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Println("Warning: No .env file found. Using default values.")
 	}
 
-	// Connect to database
 	config.ConnectDatabase()
 
-	// Initialize router
 	router := gin.Default()
-	
-	// Apply CORS middleware before any routes
+
 	router.Use(func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
@@ -37,13 +33,11 @@ func main() {
 		c.Next()
 	})
 
-	// Load routes
 	routes.UserRoutes(router)
 	routes.BoardRoutes(router)
 	routes.ColumnRoutes(router)
 	routes.TaskRoutes(router)
 
-	// Start server
 	fmt.Println("Server running on port 8081")
 	router.Run(":8081")
 }
